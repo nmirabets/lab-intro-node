4ecf66e99f648ad1c4ea6ac6272d10557e59cba3
Nicolás <nmirabets@me.com>
2021-04-20 17:46:46 +0200

    started

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => a-b );
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    
+  }
 
   max() {}
 
