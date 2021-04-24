d292105cb78b16a52f46e30cedece614e40a00a3
Nicolás <nmirabets@me.com>
2021-04-24 16:22:37 +0200

    Done!

--- index.js
+++ index.js
@@ -11,16 +11,39 @@ class SortedList {
   }
 
   get(pos) {
-    
+    if (pos>this.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
   }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.length-1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a,b) => a+b, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum()/this.length;
+  }
 }
 
 module.exports = SortedList;
